23215ab00706fccc43e85681ab652245bcf31369
James Hagen <jameshagen@jamess-air.lan>
2023-07-13 16:09:02 -0400

    add smily face

--- src/js/game.ts
+++ src/js/game.ts
@@ -30,16 +30,24 @@ export default class Game {
         if (this.canvas.getContext) {
           const ctx = this.canvas.getContext("2d");
          
-          ctx.fillStyle = "rgb(200, 200, 200)";
+          /*ctx.fillStyle = "rgb(200, 200, 200)";
           ctx.fillRect(10, 10, 50, 50);
 
           ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
-          ctx.fillRect(30, 30, 50, 50);
-
-          ctx.fillStyle = "rgba(0, 200, 0, 0.5)";
-          ctx.arc(300, 300, 50, 0, Math.PI * 2, true);
-          ctx.fill();
-
+          ctx.fillRect(30, 30, 50, 50);*/
+          ctx.fillStyle = "rgb(200, 200, 0)";
+          ctx.beginPath();
+    	  ctx.arc(75, 75, 50, 0, Math.PI * 2, true); 
+    	  ctx.fill();
+          ctx.moveTo(110, 75);
+    	  ctx.arc(75, 75, 35, 0, Math.PI, false); 
+    	  ctx.moveTo(65, 65);
+    	  ctx.arc(60, 65, 5, 0, Math.PI * 2, true); 
+    	  ctx.moveTo(95, 65);
+    	  ctx.arc(90, 65, 5, 0, Math.PI * 2, true); 
+    	  ctx.fill();
+    	  ctx.stroke();
+    	  
         } else{
 			console.log("test");
 		}
