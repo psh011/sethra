047f11b9e3c9dcd963b38133a09e2f243c00c8b8
James Hagen <jameshagen@jamess-air.lan>
2023-07-13 15:45:24 -0400

    made shapes

--- src/js/game.ts
+++ src/js/game.ts
@@ -1,4 +1,4 @@
-import Template from "../assets/images/template.png"
+/*import Template from "../assets/images/template.png"*/
 export default class Game {
 	private canvas: HTMLCanvasElement;
 	private ctx: CanvasRenderingContext2D;
@@ -21,23 +21,28 @@ export default class Game {
 
 	public draw(): void {
 
-		const img = new Image()
+		/*const img = new Image()
 		img.src = Template
 		img.onload = () => {
 			console.log("test")
 		    this.ctx.drawImage(img, 0, 0)
-		}
-        /*if (this.canvas.getContext) {
+		}*/
+        if (this.canvas.getContext) {
           const ctx = this.canvas.getContext("2d");
          
-          ctx.fillStyle = "rgb(200, 0, 0)";
+          ctx.fillStyle = "rgb(200, 200, 200)";
           ctx.fillRect(10, 10, 50, 50);
 
-          ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+          ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
           ctx.fillRect(30, 30, 50, 50);
+
+          ctx.fillStyle = "rgba(0, 200, 0, 0.5)";
+          ctx.arc(300, 300, 50, 0, Math.PI * 2, true);
+          ctx.fill();
+
         } else{
 			console.log("test");
-		}*/
+		}
     }
       
 }
